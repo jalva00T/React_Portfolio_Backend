63e569326c4688caa14fc324f420949dcbb39d50
Jake Mercedes Alvarez <76409353+jalva00T@users.noreply.github.com>
2023-01-29 13:37:09 -0600

    added 127.0.0.1 to ALLOWED_HOSTS in settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-j%8t$8l23qj#5_4r=o((^^whk4mo_&)qdnwo03-6$!(w7f5d!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://localhost:3000/', '127.0.0.1']
 
 
 # Application definition
